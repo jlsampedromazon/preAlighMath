9f8963de83d6e48234f6fa426be6c94f4ccd552d
Jose L Sampedro Mazon <jlsm@Mac.attlocal.net>
2025-01-03 16:51:19 -0800

    Clean-up graph scheduling code

--- graphScheduling.py
+++ graphScheduling.py
@@ -1,31 +1,18 @@
+
+''' 
+FIRST VERSION - Improvements for Next Version:
+    * Allow user input of Committees, Members and available Slots (currently 'hardwired')
+    * If user does not specify available slots, calculate minimum number of slots and schedule accordingly
+    * Auto-create the Conflicts Matrix based on users input of Committees and Members
+'''
+
+# SET-UP COMMITTEE CLASS AND MAIN FUNCTION
+
 class Committee:
     def __init__(self, commName, conflicts):
         self.commName = commName
         self.conflicts = conflicts
         self.slots = [1, 2, 3]
-
-
-conflictGraph = [
-    [0, 1, 1, 1, 1, 1, 0],
-    [1, 0, 0, 0, 1, 0, 0],
-    [1, 0, 0, 1, 1, 0, 0],
-    [1, 0, 1, 0, 0, 1, 1],
-    [1, 1, 1, 0, 0, 1, 0],
-    [1, 0, 0, 1, 1, 0, 1],
-    [0, 0, 0, 1, 0, 1, 0]
-]
-
-
-academicPolicy = Committee("Academic Policy", conflictGraph[0])
-boardOfGovernors = Committee("Board of Governors", conflictGraph[1])
-classroomVision = Committee("Classroom Vision", conflictGraph[2])
-dei = Committee("Diversity and Equity", conflictGraph[3])
-externalRelations = Committee("External Relations", conflictGraph[4])
-facultyDevelopment = Committee("Faculty Development", conflictGraph[5])
-graduateStudies = Committee("Graduate Studies", conflictGraph[6])
-
-allCommittees = [academicPolicy, boardOfGovernors, classroomVision, dei, externalRelations, facultyDevelopment, graduateStudies]
-
 
 def scheduleCommittees():
     
@@ -44,4 +31,27 @@ def scheduleCommittees():
     return None
 
 
+# SET-UP COMMITTEES, CONFLICTS AND AVAILABLE SLOTS
+
+conflictGraph = [
+    [0, 1, 1, 1, 1, 1, 0],
+    [1, 0, 0, 0, 1, 0, 0],
+    [1, 0, 0, 1, 1, 0, 0],
+    [1, 0, 1, 0, 0, 1, 1],
+    [1, 1, 1, 0, 0, 1, 0],
+    [1, 0, 0, 1, 1, 0, 1],
+    [0, 0, 0, 1, 0, 1, 0]
+]
+
+academicPolicy = Committee("Academic Policy", conflictGraph[0])
+boardOfGovernors = Committee("Board of Governors", conflictGraph[1])
+classroomVision = Committee("Classroom Vision", conflictGraph[2])
+dei = Committee("Diversity and Equity", conflictGraph[3])
+externalRelations = Committee("External Relations", conflictGraph[4])
+facultyDevelopment = Committee("Faculty Development", conflictGraph[5])
+graduateStudies = Committee("Graduate Studies", conflictGraph[6])
+
+allCommittees = [academicPolicy, boardOfGovernors, classroomVision, dei, externalRelations, facultyDevelopment, graduateStudies]
+
+# RUN!
 scheduleCommittees()
